c1a95b83508eef82954a512d6f81b9e0eb9772f4
edrickrenan <edrickrenan@gmail.com>
2017-08-17 15:04:49 -0300

    PostgreSQL configuration

--- knexfile.js
+++ knexfile.js
@@ -9,3 +9,14 @@ module.exports = {
     }
   }
 };
+
+// If you want to use PostgreSQL just use this configuration below and run npm instal --save pg.
+// development: {
+//   client: 'pg',
+//     connection: {
+//     host: '127.0.0.1',
+//       user: 'postgres',
+//       password: 'postgres',
+//       database: 'redmine'
+//   }
+// }
